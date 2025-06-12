efdd42643d8af4a2271a852cf36fe367604b3597
Masoud Soleimani <masoudsol@gmail.com>
2025-06-12 16:03:05 -0400

    Initial Commit

---
+++ Sake Guide/Sake_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sake_GuideApp.swift
+//  Sake Guide
+//
+//  Created by Masoud Soleimani on 2025-06-12.
+//
+
+import SwiftUI
+
+@main
+struct Sake_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
